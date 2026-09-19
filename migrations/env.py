7892d4087c743831cc1ad584dca30757ea9290d0
Ritika <178850857+ritika-s05@users.noreply.github.com>
2026-09-19 19:26:43 -0400

    configure Alembic metadata and database connection
    Load environment variables from a .env file and set the database URL for Alembic migrations.

--- migrations/env.py
+++ migrations/env.py
@@ -4,11 +4,25 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+from app.database.base import Base
+from app.models.llm_req import LLMRequest
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / "backend" / ".env")
 
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not configured.")
+
+config.set_main_option("sqlalchemy.url", database_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -18,7 +32,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
